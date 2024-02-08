72500b5ec26d91ef71b54a23aae5556eb95058ea
IshanGupta <guptaishan506@gmail.com>
2024-02-08 23:04:44 +0530

    08-02-24

---
+++ Add-2-number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+// Declare Integers
+    int a,b,sum ; 
+
+// Get Integers                                            
+    printf("Enter 2 digit =");                    
+    scanf("%d %d",&a ,&b);
+
+// Calculate the sum
+    sum=a+b;
+
+// Print Solution
+    printf("\nSum of the digits=%d",sum);
+    return 0;
+
+}
