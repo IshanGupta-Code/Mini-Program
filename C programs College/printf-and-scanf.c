0723411049de04323ce056afa88fb2beb75bce9c
IshanGupta <guptaishan506@gmail.com>
2024-02-10 17:39:12 +0530

    10-02-24

--- C programs College/printf-and-scanf.c
+++ C programs College/printf-and-scanf.c
@@ -1,4 +1,4 @@
-// Given two integer 20 and 10 , write a program to add and subtract these numbers and show output as   20+10 = 30, and  20-10 =10.
+// Given two integer 20 and 10  write a program to add and subtract these numbers and show output as   20+10 = 30, and  20-10 =10.
 
 #include<stdio.h>
 int main(){
